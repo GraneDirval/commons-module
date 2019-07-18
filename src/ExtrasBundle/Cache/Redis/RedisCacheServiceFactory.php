c1b851691c175bfdf1dbf3d1ad86ddd939012bac
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-07-18 14:16:00 +0300

    Cache adjustments

--- src/ExtrasBundle/Cache/Redis/RedisCacheServiceFactory.php
+++ src/ExtrasBundle/Cache/Redis/RedisCacheServiceFactory.php
@@ -9,9 +9,9 @@ use Symfony\Component\Cache\Exception\InvalidArgumentException;
 
 class RedisCacheServiceFactory implements ICacheServiceFactory
 {
-    const PLACEHOLDER_DATABASE       = 1;
+    const PLACEHOLDER_DATABASE = 1;
     const USER_SUBSCRIPTION_DATABASE = 1;
-    const COUNTERS_DATABASE          = 2;
+    const COUNTERS_DATABASE = 2;
 
     /**
      * @var RedisConnectionProvider
@@ -33,53 +33,9 @@ class RedisCacheServiceFactory implements ICacheServiceFactory
     public function __construct(RedisConnectionProvider $connectionProvider, string $namespace)
     {
         $this->connectionProvider = $connectionProvider;
-        $this->namespace = $namespace;
+        $this->namespace          = $namespace;
     }
 
-    /**
-     * @param array $options
-     *
-     * @return ICacheService
-     * @throws InvalidArgumentException
-     */
-    public function createTranslationCacheService(array $options = []): ICacheService
-    {
-        return $this->createService(self::PLACEHOLDER_DATABASE, $options, sprintf('%s_%s_', $this->namespace, 'placheolders'));
-    }
-
-    /**
-     * @param array $options
-     *
-     * @return ICacheService
-     * @throws InvalidArgumentException
-     */
-    public function createUserSubscriptionCacheService(array $options = []): ICacheService
-    {
-        return $this->createService(self::USER_SUBSCRIPTION_DATABASE, $options, sprintf('%s_%s_', $this->namespace, 'subscriptions'));
-    }
-
-    /**
-     * @return \Predis\Client|\Redis|\RedisCluster
-     */
-    public function createCounterConnection()
-    {
-        $connection = $this->connectionProvider->create(self::COUNTERS_DATABASE);
-        return $connection;
-    }
-
-    /**
-     * @param string $database
-     * @param array  $options
-     * @param string $namespace
-     *
-     * @return ICacheService
-     */
-    private function createService(string $database, array $options = [], $namespace = 'default'): ICacheService
-    {
-        $connection = $this->connectionProvider->create($database, $options);
-
-        return new RedisCacheService(new RedisAdapter($connection, $namespace));
-    }
 
     /**
      * @param int    $database
