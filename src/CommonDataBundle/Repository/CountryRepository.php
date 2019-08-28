f70247a248cce4d0572a3022024fa955ef9f96f1
Daniel Kleba <klebadanial@gmail.com>
2019-08-28 13:55:04 +0300

    remove method from country rep.

--- src/CommonDataBundle/Repository/CountryRepository.php
+++ src/CommonDataBundle/Repository/CountryRepository.php
@@ -4,24 +4,10 @@ namespace CommonDataBundle\Repository;
 
 
 use CommonDataBundle\Repository\Interfaces\CountryRepositoryInterface;
-use Doctrine\ORM\AbstractQuery;
-use Doctrine\ORM\EntityRepository;
 
 /**
  * Class CountryRepository
  */
 class CountryRepository extends EntityRepository implements CountryRepositoryInterface
 {
-    /**
-     * @return array
-     */
-    public function findEnabledCarriersCountryCodes(): array
-    {
-        $qb = $this->createQueryBuilder('v');
-        $qb->innerJoin('carrier', 'c','WITH', 'c.countryCode = v.countryCode AND c.published = true')
-            ->select('v')
-            ->groupBy('c.countryCode');
-
-        return $qb->getQuery()->getResult(AbstractQuery::HYDRATE_OBJECT);
-    }
 }
--- src/CommonDataBundle/Repository/Interfaces/CountryRepositoryInterface.php
+++ src/CommonDataBundle/Repository/Interfaces/CountryRepositoryInterface.php
@@ -6,5 +6,4 @@ namespace CommonDataBundle\Repository\Interfaces;
 
 interface CountryRepositoryInterface
 {
-    public function findEnabledCarriersCountryCodes(): array;
 }
